0db5d3cd940e0ee2590f724de6438c58bc081409
danmgs <dnlnguyen@gmail.com>
2019-12-02 14:44:19 +0100

    Update stop_server.sh
    fix
    https://stackoverflow.com/questions/2268104/command-not-found-error-in-bash-variable-assignment

--- app/scripts/stop_server.sh
+++ app/scripts/stop_server.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ## AWS AMI Linux 1 ##
-# isExistApp = `pgrep httpd`
+# isExistApp=`pgrep httpd`
 # if [[ -n  $isExistApp ]]; then
 #     service httpd stop
 # fi
 
 ## AWS AMI Linux 2 ##
-isExistApp = `pgrep httpd`
+isExistApp=`pgrep httpd`
 if [[ -n  $isExistApp ]]; then
     systemctl stop httpd.service
 fi
 
 # https://www.linode.com/docs/tools-reference/tools/use-killall-and-kill-to-stop-processes-on-linux/
 
-isExistProcess = `pgrep dotnet`
+isExistProcess=`pgrep dotnet`
 if [[ -n  $isExistProcess ]]; then
     killall -KILL dotnet
 fi
