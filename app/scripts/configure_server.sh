0563cb41f2cbccf68a2c606ceeb62f87703d1643
danmgs <dnlnguyen@gmail.com>
2019-12-02 12:49:26 +0100

    Update configure_server.sh
    fix "not such file in directory"

--- app/scripts/configure_server.sh
+++ app/scripts/configure_server.sh
@@ -7,7 +7,7 @@ if [ -f /etc/httpd/conf.d/default-site.conf ]; then
     echo "delete"
 fi
 
-cat <<EOF > /etc/httpd/conf.d/default-site.conf
+cat <<EOF >> /etc/httpd/conf.d/default-site.conf
  <VirtualHost *:80>
   ProxyPass / http://127.0.0.1:5000/
   ProxyPassReverse / http://127.0.0.1:5000/
