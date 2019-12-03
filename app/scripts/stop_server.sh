599f32a90a50bd0c2ff6facde45192a7bb8e6dfc
danmgs <dnlnguyen@gmail.com>
2019-12-03 15:02:32 +0100

    Update stop_server.sh
    Debugging

--- app/scripts/stop_server.sh
+++ app/scripts/stop_server.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+pgrep httpd
+
 ## AWS AMI Linux 2 ##
 isExistApp=`pgrep httpd`
 if [[ -n  $isExistApp ]]; then
     systemctl stop httpd.service
     echo "- httpd.service stopped"
+else
+    echo "- no httpd.service to stop"
 fi
 
 # https://www.linode.com/docs/tools-reference/tools/use-killall-and-kill-to-stop-processes-on-linux/
+
+pgrep dotnet
 
 isExistProcess=`pgrep dotnet`
 if [[ -n  $isExistProcess ]]; then
     killall -KILL dotnet
     echo "- dotnet process killed"
+else
+    echo "- no dotnet process to stop"
 fi
